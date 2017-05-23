40e5f2bf6144fca0d0d338013e7eaf55b07fd007
Luk Red <525595+Erdk@users.noreply.github.com>
2017-05-23 22:06:18 +0200

    rofi wrapper

---
+++ bin/rofi.sh
@@ -0,0 +1,5 @@
+rofi -combi-modi window,drun,run -show combi -modi combi \
+  -color-window "#0d1926, #002b36, #000000" \
+  -color-normal "#0d1926, #268bd2, #0d1926, #268bd2, #0d1926" # \
+# -color-active"#0d1926, #268bd2, #0d1926, #268bd2, #0d1926" \
+# -color-urgent "#0d1926, #268bd2, #0d1926, #268bd2, #0d1926"
