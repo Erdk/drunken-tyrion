e66fc78290f80c72705e43ece1ca2cea7dfce79c
Luk Red <525595+Erdk@users.noreply.github.com>
2017-07-02 13:50:30 +0200

    Golang: update/install dev tools

---
+++ bin/go_update_tools.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v github.com/zmb3/gogetdoc
+go get -u -v github.com/golang/lint/golint
+go get -u -v github.com/ramya-rao-a/go-outline
+go get -u -v sourcegraph.com/sqs/goreturns
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v github.com/tpng/gopkgs
+go get -u -v github.com/acroca/go-symbols
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v github.com/cweill/gotests/...
+go get -u -v golang.org/x/tools/cmd/godoc
+go get -u -v github.com/fatih/gomodifytags
+go get -u -v github.com/josharian/impl
