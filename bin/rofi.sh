855c3bfb86a33140d4758241c48b9bfda195a77f
Luk Red <525595+Erdk@users.noreply.github.com>
2017-07-18 11:11:46 +0200

    rofi: dark-red scheme

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -1,5 +1,5 @@
 rofi -combi-modi window,drun,run -show combi -modi combi \
-  -color-window "#0d1926, #002b36, #000000" \
-  -color-normal "#0d1926, #268bd2, #0d1926, #268bd2, #0d1926" # \
-# -color-active"#0d1926, #268bd2, #0d1926, #268bd2, #0d1926" \
+  -color-window "#191919, #191919, #bac7d4" \
+  -color-normal "#191919, #bac7d4, #212121, #bac7d4, #191919" \
+  -color-active "#191919, #A00000, #212121, #A00000, #bac7d4"
 # -color-urgent "#0d1926, #268bd2, #0d1926, #268bd2, #0d1926"
